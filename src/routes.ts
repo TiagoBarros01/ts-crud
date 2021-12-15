a42bc71ed775a369f575e8f6c3d576d38870b173
Tiago Barros <tbarros1337@hotmail.com>
2021-12-14 23:31:44 -0300

    feat: Get all categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
 
 export const routes = Router();
 
 const createCategoryController = new CreateCategoryController().handle;
+const getAllCategoriesController = new GetAllCategoriesController().handle;
 
 routes.post('/categories', createCategoryController)
+routes.get('/categories', getAllCategoriesController)
