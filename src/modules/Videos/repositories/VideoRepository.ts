3df371d1f843be75589e95f30dcdb7a6f4afd7de
Tiago Barros <tbarros1337@hotmail.com>
2022-02-13 22:34:59 -0300

    fix: Videos repository without constructor

--- src/modules/Videos/repositories/VideoRepository.ts
+++ src/modules/Videos/repositories/VideoRepository.ts
@@ -1,16 +1,20 @@
-import { Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { CreateVideoDTO } from "../dtos/CreateVideoRepositoryDTO";
-import { Video } from "../entities/Video";
+import { Videos } from "../entities/Videos";
 import { VideoRepositoryData } from "./types/VideoRepositoryData";
 
 export class VideoRepository implements VideoRepositoryData {
-  ormRepository: Repository<Video>;
+  public ormRepository: Repository<Videos>;
 
-  public async save(video: Video): Promise<Video> {
+  constructor() {
+    this.ormRepository = getRepository(Videos);
+  }
+
+  public async save(video: Videos): Promise<Videos> {
     return this.ormRepository.save(video);
   }
 
-  public async create(data: CreateVideoDTO): Promise<Video> {
+  public async create(data: CreateVideoDTO): Promise<Videos> {
     const video = this.ormRepository.create(data);
 
     await this.ormRepository.save(video);
@@ -18,17 +22,19 @@ export class VideoRepository implements VideoRepositoryData {
     return video;
   }
 
-  public async remove(videos: Video[]): Promise<void> {
+  public async remove(videos: Videos[]): Promise<void> {
     await this.ormRepository.remove(videos);
   }
 
-  public async findAll(): Promise<Video[]> {
-    const videos = await this.ormRepository.find();
+  public async findAll(): Promise<Videos[]> {
+    const videos = await this.ormRepository.find({
+      relations: ['category']
+    });
 
     return videos;
   }
 
-  public async findByCategoryId(category_id: string): Promise<Video> {
+  public async findByCategoryId(category_id: string): Promise<Videos> {
     const video = await this.ormRepository.findOne({
       where: {
         category_id,
@@ -38,13 +44,13 @@ export class VideoRepository implements VideoRepositoryData {
     return video;
   }
 
-  public async findById(id: string): Promise<Video> {
+  public async findById(id: string): Promise<Videos> {
     const video = await this.ormRepository.findOne(id);
 
     return video;
   }
 
-  public async findByName(name: string): Promise<Video> {
+  public async findByName(name: string): Promise<Videos> {
     const video = await this.ormRepository.findOne({
       where: {
         name,
